f67d3a39cd460111096cca856f6eaac1e189d154
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2023-01-06 21:53:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 import {fetchMovieAvailability,fetchMovieList} from "./api.js"
+let movies =[];
+
+function showMovies(){
+   
+}
+
+async function getMovies(){
+  const response = await fetchMovieList();
+//   movies = await response.json();.
+  showMovies();
+}
+
+getMovies();
