d88c65cb12f8bf5cf44ff80373d4a95e13ac85b9
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2023-01-06 22:12:02 +0530

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 import {fetchMovieAvailability,fetchMovieList} from "./api.js"
 let movies =[];
+let loading = true;
+const loaderElement = document.getElementById("loader");
+
+function showLoader(){
+  loaderElement.className="d-block";
+}
+function hideLoader(){
+  loaderElement.className="d-none";
+}
 
 function showMovies(){
-   
+  const mainElement = document.querySelector("main");
+  const divElement = document.querySelector("div");
+  divElement.className="movie-holder";
+  mainElement.appendChild(divElement);
+  
+  for(let movie of movies){
+    const movieElement = document.createElement("div");
+    movieElement.innerHTML = `<a class="movie- link" href="/${movie.name}">
+    <div class="movie" data- d="${movie.name}">
+    <div class="movie-img-wrapper">
+      <img src="${movie.imgUrl}" width="300px">
+    </div>
+    <h4>${movie.name}</h4>
+    </div>
+    </a>`
+  }
 }
 
 async function getMovies(){
